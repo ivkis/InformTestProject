fcd49586e6112792cc7eedc22a5c08e9e53c3a59
ivkis <ivanlazarevkis@gmail.com>
2017-08-19 17:28:24 +0300

    API.getPersonInfo should call callback on main thread

--- InformTestProject/API.swift
+++ InformTestProject/API.swift
@@ -15,23 +15,28 @@ class API {
     static let shared = API()
 
     func getPersonInfo(callback: @escaping (Person?) -> Void) {
+        let asyncCallback = { (person: Person?) in
+            DispatchQueue.main.async {
+                callback(person)
+            }
+        }
         URLSession.shared.dataTask(with: Constants.urlPersonInfo) { data, response, error in
             guard let data = data, error == nil else {
-                callback(nil)
+                asyncCallback(nil)
                 return
             }
             do {
                 let json = try JSONSerialization.jsonObject(with: data)
                 guard let results = json as? [String: AnyObject] else {
                     print("Non-dictionary response")
-                    callback(nil)
+                    asyncCallback(nil)
                     return
                 }
                 let personInfo = Person(json: results)
-                callback(personInfo)
+                asyncCallback(personInfo)
             } catch {
                 print(error)
-                callback(nil)
+                asyncCallback(nil)
             }
         }.resume()
     }
