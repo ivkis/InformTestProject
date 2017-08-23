83dfb612b0db88f85976c40fafb3511e4b5c31b1
ivkis <ivanlazarevkis@gmail.com>
2017-08-23 18:18:33 +0300

    When clicked button change counter value

--- InformTestProject/CounterViewController.swift
+++ InformTestProject/CounterViewController.swift
@@ -12,6 +12,14 @@ import UIKit
 class CounterViewController: UIViewController {
 
     @IBOutlet weak var valueCounterLabel: UILabel!
+    var currentValue: Int? {
+        get {
+            return Int(valueCounterLabel.text ?? "invalid")
+        }
+        set {
+            self.valueCounterLabel.text = newValue == nil ? "" : String(newValue!)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +31,33 @@ class CounterViewController: UIViewController {
     func loadData() {
         API.shared.getCounter { valueCounter in
             if let valueCounter = valueCounter {
-                self.valueCounterLabel.text = String(valueCounter)
+                self.currentValue = valueCounter
             } else {
                 print("error counter")
             }
         }
+    }
+
+    func sendData() {
+        guard let currentValue = Int(valueCounterLabel.text ?? "invalid") else {
+            return
+        }
+    }
+
+    @IBAction func incrementButtonTapped(_ sender: Any) {
+        guard let currentValue = currentValue else {
+            return
+        }
+        self.currentValue = currentValue + 1
+        // sendData later
+    }
+
+
+    @IBAction func decrementButtonTapped(_ sender: Any) {
+        guard let currentValue = currentValue else {
+            return
+        }
+        self.currentValue = currentValue - 1
+        // sendData later
     }
 }
